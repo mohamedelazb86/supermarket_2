5bbde2dc9ecf2c758989d54087f311aa84a793b3
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2025-04-23 15:21:26 +0200

    add new profile if we create new user by signals

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from utils.generate_code import generate_code
 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 class Profile(models.Model):
     user=models.OneToOneField(User,related_name='profile_user',on_delete=models.CASCADE)
     image=models.ImageField(upload_to='photo_user')
@@ -9,5 +12,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+    
+# create new profile if we create new user by signals
+@receiver(post_save,sender=User)
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
+
     
 
