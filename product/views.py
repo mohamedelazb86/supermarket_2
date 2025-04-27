decaa69b9004a89dbcb7c3af507399aa63ac53c3
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2025-04-27 19:00:13 +0200

    add brand and brand detail

--- product/views.py
+++ product/views.py
@@ -47,6 +47,32 @@ class Brand_List(ListView):
     queryset=Brand.objects.all().annotate(product_count=Count('product_brand'))
     
 
-class Brand_Detail(DetailView):
-    model=Brand
+# class Brand_Detail(DetailView):
+#     model=Brand
+#     template_name='product/brand_detail.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["products"] = Product.objects.filter(brand=self.get_object())
+#         return context
+    
+
+class Brand_Detail(ListView):
+    model=Product
     template_name='product/brand_detail.html'
+    paginate_by=5
+
+    def get_queryset(self):
+        brand=Brand.objects.get(slug=self.kwargs['slug'])
+        queryset=Product.objects.filter(brand=brand)
+        return queryset
+    
+
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_brand'))[0]
+        return context
+    
+    
+
+  
